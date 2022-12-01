299f062d7fbda945cab61dc585500f7fcfed2c5e
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2022-12-01 14:24:48 -0600

    Finished part 2 of day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,19 +3,31 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut max: u32 = 0;
+    let mut max: [u32; 3] = [0,0,0];
     let mut current: u32 = 0;
     let lines = read_lines("./in.txt").unwrap();
     for line in lines {
         current = match line.parse::<u32>() {
             Ok(num) => num + current,
-            _ => 0,
+            _ => {
+                if current > max[0] {
+                    max[0] = current;
+                    for i in 1..max.len(){
+                        if max[i-1] > max[i] {
+                            current = max[i];
+                            max[i] = max[i-1];
+                            max[i-1] = current;
+                        }
+                    }
+                }
+                0
+            },
         };
-        if current > max {
-            max = current;
-        }
     }
-    println!("{}",&max);
+
+    let sum: u32 = max.iter().sum();
+
+    println!("{}",&sum);
 }
 
 // The output is wrapped in a Result to allow matching on errors

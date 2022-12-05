bbc19cb7c6cda20318b9ba0b5d1351b41a7c98e6
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2022-12-05 10:52:41 -0600

    Finished part 1 of day 5

---
+++ day5/src/main.rs
@@ -0,0 +1,61 @@
+fn main() {
+    let input: Vec<&str> = include_str!("in.txt").split("\n\n").collect();
+
+    let (ship, instructions) = (input[0], input[1]);
+
+    let ship: Vec<&str> = ship.split("\n").collect();
+
+    let number_of_stacks = ship
+        .last()
+        .copied()
+        .unwrap()
+        .split_whitespace()
+        .map(str::parse::<usize>)
+        .filter(|r| r.is_ok())
+        .last()
+        .unwrap()
+        .unwrap();
+
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+
+    for _ in 0..number_of_stacks {
+        stacks.push(Vec::new())
+    }
+
+    ship[..ship.len() - 1]
+        .iter()
+        .copied()
+        .rev()
+        .for_each(|line| {
+            for (i, c) in line
+                .chars()
+                .enumerate()
+                .filter(|(i, _)| (i + 3) % 4 == 0)
+                .map(|(_, c)| c)
+                .enumerate()
+                .filter(|(_, c)| c.is_ascii_uppercase())
+            {
+                stacks[i].push(c);
+            }
+        });
+
+    instructions.split("\n").for_each(|line| {
+        let instruction_parts: Vec<usize> = line
+            .split_whitespace()
+            .map(str::parse::<usize>)
+            .filter(|r| r.is_ok())
+            .map(|r| r.unwrap())
+            .collect();
+        
+        for _ in 0..instruction_parts[0] {
+            let value = stacks[instruction_parts[1] - 1].pop();
+
+            stacks[instruction_parts[2] - 1].push(value.unwrap());
+        }
+    });
+
+    for i in 0..number_of_stacks {
+        print!("{}", stacks[i].pop().unwrap());
+    }
+    println!("");
+}

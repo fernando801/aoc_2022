72d93858b7e02e603324f4fa653756f90b2f7b77
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2022-12-07 20:33:11 -0600

    Finished part 2 of day 7

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -1,3 +1,5 @@
+use priority_queue::DoublePriorityQueue;
+
 fn part1() {
     let input = include_str!("input.txt");
     let input = &format!("{}{}", input, "\n$ cd ..")[..];
@@ -38,7 +40,51 @@ fn part1() {
     println!("{}", &total);
 }
 
-fn part2() {}
+fn part2() {
+    let input = include_str!("input.txt");
+    let input = &format!("{}{}", input, "\n$ cd ..")[..];
+
+    let mut stack: Vec<usize> = Vec::new();
+    let mut dir_sizes = DoublePriorityQueue::new();
+
+    for line in input.lines() {
+        if line == "$ ls" {
+            continue;
+        }
+
+        if line.starts_with("$ cd ") {
+            let dir = &line[5..];
+
+            if dir == ".." {
+                let size = stack.pop().unwrap();
+                *stack.last_mut().unwrap() += size;
+                dir_sizes.push(size, size);
+            } else {
+                stack.push(0);
+            }
+            continue;
+        }
+
+        if let Ok(size) = line.split_once(" ").unwrap().0.parse::<usize>() {
+            *stack.last_mut().unwrap() += size;
+        }
+    }
+
+    let root_dir_size = stack.pop().unwrap();
+    dir_sizes.push(root_dir_size, root_dir_size);
+
+    let space_to_free = root_dir_size - 40_000_000;
+
+    let size_of_dir_to_free = loop {
+        let min = dir_sizes.pop_min().unwrap();
+
+        if min.0 >= space_to_free{
+            break min.0;
+        }
+    };
+
+    println!("{}", &size_of_dir_to_free);
+}
 
 fn main() {
     println!("Part 1:");

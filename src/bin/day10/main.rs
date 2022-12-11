7a44e8041c23354aab2965c0ba264b5aafd78b40
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2022-12-10 21:31:27 -0600

    Finished part 2 of day 10

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -26,7 +26,38 @@ fn part1() {
     println!("{}", sum);
 }
 
-fn part2() {}
+fn part2() {
+    let input = include_str!("input.txt");
+
+    let mut cicle: i32 = 1;
+    let mut x: i32 = 1;
+
+    let mut screen = [['.'; 40]; 6];
+
+    for line in input.lines() {
+        if let Some((_, amount)) = line.split_once(" ") {
+            for _ in 0..2 {
+                if ((cicle - 1) % 40 - x).abs() <= 1 {
+                    let c = cicle as usize;
+                    screen[(c - 1) / 40][(c - 1) % 40] = '#';
+                }
+                cicle += 1;
+            }
+            x += amount.parse::<i32>().unwrap();
+        } else {
+            if ((cicle - 1) % 40 - x).abs() <= 1 {
+                let c = cicle as usize;
+                screen[(c - 1) / 40][(c - 1) % 40] = '#';
+            }
+            cicle += 1;
+        }
+    }
+
+    screen.iter().for_each(|line| {
+        line.iter().for_each(|c| print!("{}", c));
+        println!();
+    });
+}
 
 fn main() {
     println!("Part 1:");

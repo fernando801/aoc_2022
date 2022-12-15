202ea5b0426524e50517bff48d9960c9f221e1ce
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2022-12-14 22:39:05 -0600

    Finished part 2 of day 14

--- src/bin/day14/main.rs
+++ src/bin/day14/main.rs
@@ -84,7 +84,88 @@ fn part1() {
     println!("{}", sand_units);
 }
 
-fn part2() {}
+fn part2() {
+    let input = include_str!("input.txt");
+
+    let mut map: HashMap<(i32, i32), char> = HashMap::new();
+
+    let mut max_y = i32::MIN;
+
+    for line in input.lines() {
+        let vrtx: Vec<(i32, i32)> = line
+            .split(" -> ")
+            .map(|pair| {
+                let (x, y) = pair.split_once(",").unwrap();
+                (x.parse().unwrap(), y.parse().unwrap())
+            })
+            .collect();
+
+        for i in 1..vrtx.len() {
+            max_y = cmp::max(cmp::max(vrtx[i - 1].1, vrtx[i].1), max_y);
+
+            if vrtx[i - 1].0 == vrtx[i].0 {
+                let x = vrtx[i].0;
+                let y1 = vrtx[i - 1].1;
+                let y2 = vrtx[i].1;
+
+                for y in cmp::min(y1, y2)..cmp::max(y1, y2) + 1 {
+                    map.insert((x, y), '#');
+                }
+            } else {
+                let y = vrtx[i].1;
+                let x1 = vrtx[i - 1].0;
+                let x2 = vrtx[i].0;
+
+                for x in cmp::min(x1, x2)..cmp::max(x1, x2) + 1 {
+                    map.insert((x, y), '#');
+                }
+            }
+        }
+    }
+
+    let mut sand_units = 0;
+
+    'sand_drop: loop {
+        let mut sand = (500, 0);
+
+        loop {
+            if sand.1 > max_y {
+                map.insert(sand, 'o');
+                sand_units += 1;
+                if sand == (500, 0) {
+                    break 'sand_drop;
+                } else {
+                    break;
+                }
+            }
+
+            if let Entry::Vacant(_) = map.entry((sand.0, sand.1 + 1)) {
+                sand = (sand.0, sand.1 + 1);
+                continue;
+            }
+
+            if let Entry::Vacant(_) = map.entry((sand.0 - 1, sand.1 + 1)) {
+                sand = (sand.0 - 1, sand.1 + 1);
+                continue;
+            }
+
+            if let Entry::Vacant(_) = map.entry((sand.0 + 1, sand.1 + 1)) {
+                sand = (sand.0 + 1, sand.1 + 1);
+                continue;
+            }
+
+            map.insert(sand, 'o');
+            sand_units += 1;
+            if sand == (500, 0) {
+                break 'sand_drop;
+            } else {
+                break;
+            }
+        }
+    }
+
+    println!("{}", sand_units);
+}
 
 fn main() {
     println!("Part 1:");

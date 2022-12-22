f5c6346403cda3ba8045398c5566b83bca7e7abc
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2022-12-22 00:51:37 -0600

    Finished part 2 of day 19

--- src/bin/day19/main.rs
+++ src/bin/day19/main.rs
@@ -19,11 +19,6 @@ fn dfs(
     }
 
     let key = (current.time_left, current.robots, current.resources);
-    // println!("{:?}", key);
-
-    // let mut guess = String::new();
-    // std::io::stdin()
-    //     .read_line(&mut guess);
 
     if let Some(cached_value) = cache.get(&key).copied() {
         return cached_value;
@@ -67,7 +62,10 @@ fn dfs(
         }
 
         for i in 0..3 {
-            resources[i] = cmp::min(resources[i], max_spend[i] * time_left);
+            resources[i] = cmp::min(
+                resources[i],
+                max_spend[i] * time_left - robots[i] * (time_left - 1),
+            );
         }
 
         let new_state = State {
@@ -129,7 +127,51 @@ fn part1() {
     println!("{}", quality_level_sum);
 }
 
-fn part2() {}
+fn part2() {
+    let input = include_str!("input.txt");
+
+    let re = Regex::new(r"(\d+) (\w+)").unwrap();
+
+    let mut max_spend = [0, 0, 0];
+
+    let mut max_product = 1;
+
+    for line in input.lines().take(3) {
+        let mut blueprint: Vec<Vec<(i32, usize)>> = Vec::new();
+        for robot_costs in line.split(". ") {
+            let mut recipe: Vec<(i32, usize)> = Vec::new();
+            for (amount, mineral) in re
+                .find_iter(robot_costs)
+                .map(|s| s.as_str().split_once(" ").unwrap())
+            {
+                let amount: i32 = amount.parse().unwrap();
+                let mineral: usize = match mineral {
+                    "ore" => 0,
+                    "clay" => 1,
+                    "obsidian" => 2,
+                    _ => panic!("No such mineral"),
+                };
+
+                max_spend[mineral] = cmp::max(max_spend[mineral], amount);
+                recipe.push((amount, mineral));
+            }
+
+            blueprint.push(recipe);
+        }
+
+        let mut cache = HashMap::new();
+
+        let start = State {
+            time_left: 32,
+            robots: [1, 0, 0, 0],
+            resources: [0, 0, 0, 0],
+        };
+
+        max_product *= dfs(&blueprint, max_spend, start, &mut cache);
+    }
+
+    println!("{}", max_product);
+}
 
 fn main() {
     println!("Part 1:");
